4b131b96e3485ad6856ac6eb139eed541abcda3b
Gage Reynolds <gage_reynolds@baylor.edu>
2022-05-03 13:12:37 -0500

    caught up with Django III

--- pizzas/views.py
+++ pizzas/views.py
@@ -5,3 +5,16 @@ from .models import Pizza, Topping
 # Create your views here.
 def index(request):
     return render(request, 'pizzas/index.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.all()
+    context = {'pizzas': pizzas}
+
+    return render(request, 'pizzas/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id = pizza_id)
+    toppings = pizza.topping_set.all().order_by('pizza')
+    context = {'pizza': pizza, 'toppings':toppings}
+
+    return render(request, 'pizzas/pizza.html', context)
